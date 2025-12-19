2c045318663dcc897829bf7ef4fa5352592bb697
ewenlogiciel <ewen.davanzo@gmail.com>
2025-12-19 15:12:13 +0100

    feat: add VichUploaderBundle

--- src/Entity/MediaObject.php
+++ src/Entity/MediaObject.php
@@ -1,4 +1,5 @@
 <?php
+// src/Entity/MediaObject.php
 
 namespace App\Entity;
 
@@ -7,7 +8,8 @@ use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Post;
-use ApiPlatform\OpenApi\Model;
+use App\Controller\CreateMediaObjectAction;
+use App\State\MediaObjectProcessor;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Serializer\Annotation\Groups;
@@ -17,48 +19,34 @@ use Vich\UploaderBundle\Mapping\Annotation as Vich;
 #[Vich\Uploadable]
 #[ORM\Entity]
 #[ApiResource(
-    normalizationContext: ['groups' => ['media_object:read']],
     types: ['https://schema.org/MediaObject'],
-    outputFormats: ['jsonld' => ['application/ld+json']],
     operations: [
         new Get(),
         new GetCollection(),
         new Post(
-            inputFormats: ['multipart' => ['multipart/form-data']],
-            openapi: new Model\Operation(
-                requestBody: new Model\RequestBody(
-                    content: new \ArrayObject([
-                        'multipart/form-data' => [
-                            'schema' => [
-                                'type' => 'object',
-                                'properties' => [
-                                    'file' => [
-                                        'type' => 'string',
-                                        'format' => 'binary'
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ])
-                )
-            )
+            controller: CreateMediaObjectAction::class,
+            deserialize: false,
+            validationContext: ['groups' => ['Default', 'media_object_create']]
         )
-    ]
+    ],
+    normalizationContext: ['groups' => ['media_object:read']],
+    processor: MediaObjectProcessor::class
 )]
 class MediaObject
 {
-    #[ORM\Id, ORM\Column, ORM\GeneratedValue]
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
     private ?int $id = null;
 
-    #[ApiProperty(types: ['https://schema.org/contentUrl'], writable: false)]
+    #[ApiProperty(types: ['https://schema.org/contentUrl'])]
     #[Groups(['media_object:read'])]
     public ?string $contentUrl = null;
 
     #[Vich\UploadableField(mapping: 'media_object', fileNameProperty: 'filePath')]
-    #[Assert\NotNull]
+    #[Assert\NotNull(groups: ['media_object_create'])]
     public ?File $file = null;
 
-    #[ApiProperty(writable: false)]
     #[ORM\Column(nullable: true)]
     public ?string $filePath = null;
 
