36d05b380a6273d25b9ffc97a7161cf9f807daa6
Github MMI3 <github@mmi-troyes.fr>
2025-12-23 20:52:42 +0000

    fix: comment and user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -14,6 +14,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
@@ -32,9 +33,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['comment:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
+    #[Groups(['comment:read'])]
     private ?string $email = null;
 
     #[ORM\Column]
